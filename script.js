75b65f8c6e4848a1b4c1c2bdbfb506e2b34ef721
Jalfaro1010 <Joshua.alfaro@gmail.com>
2021-09-21 19:16:06 -0400

    variable exercise complete

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// 1. Variable type used on a variable taht will not change
+// Const
+
+//2
+// let num = 20;
+
+//3
+num = 15;
+
+//4
+num = num + 7;
+
+//5
+num--;
+
+//6
+
+
+//7
+let blankundefined;
+
+//8
+console.log(blankundefined)
+
+//9
+let blanknull;
+
+// console.log(num)
